63e60ce95f9273d85d892d072943c1dd87de2826
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-08 01:14:31 -0300

    adicionando dependency

--- src/main/java/com/starking/jobs/BatchConfig.java
+++ src/main/java/com/starking/jobs/BatchConfig.java
@@ -1,8 +1,15 @@
 package com.starking.jobs;
 
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author pedroRhamon
  */
+
+@Configuration
 public class BatchConfig {
+	
+	private JobRepository jobRepository;
 
 }
