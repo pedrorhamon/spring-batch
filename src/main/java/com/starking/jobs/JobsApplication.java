97d3f8fad4818aa0dc46fcbe1f962134602eb3a0
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-09 00:33:17 -0300

    dando close na main

--- src/main/java/com/starking/jobs/JobsApplication.java
+++ src/main/java/com/starking/jobs/JobsApplication.java
@@ -2,12 +2,14 @@ package com.starking.jobs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JobsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobsApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(JobsApplication.class, args);
+		run.close();
 	}
 
 }
