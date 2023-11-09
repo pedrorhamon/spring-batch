f34b7c599d8de3a41bbca8ab379b82fdf3a260b3
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-08 22:03:27 -0300

    criando construtor

--- src/main/java/com/starking/jobs/BatchConfig.java
+++ src/main/java/com/starking/jobs/BatchConfig.java
@@ -2,6 +2,8 @@ package com.starking.jobs;
 
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author pedroRhamon
@@ -11,5 +13,13 @@ import org.springframework.context.annotation.Configuration;
 public class BatchConfig {
 	
 	private JobRepository jobRepository;
+	private PlatformTransactionManager transactionManager;
+	private RestTemplate restTemplate;
+	
+	public BatchConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager, RestTemplate restTemplate) {
+		this.jobRepository = jobRepository;
+		this.transactionManager = transactionManager;
+		this.restTemplate = restTemplate;
+	}
 
 }
